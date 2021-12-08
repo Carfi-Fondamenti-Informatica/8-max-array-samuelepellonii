b34dea603d3692e400ec1d7c2aa133d6a9d25ab2
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-12-08 11:55:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+
+int main() {
+    int dim;
+    float max=0;
+    cin >> dim;
+    float a[dim];
+
+    for (int i=0;i<dim;i++){
+        float valore=0;
+        cin >> valore;
+        a[i]=valore;
+    }
+    cout << maggiore (a,dim,max);
+    return 0;
 }
