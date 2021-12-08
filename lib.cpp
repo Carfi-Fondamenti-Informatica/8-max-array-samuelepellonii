d1faf399b7036982538c09a5d523bb7922eed00e
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-12-08 11:54:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
 
+
+#include "lib.h"
+
+float maggiore ( float a[], int dim, float max) {
+
+    for(int i=0; i<dim;i++){
+            if(i==0){
+                max=a[i];
+
+            } else if(i>0 and a[i]>max) {
+                max=a[i];
+
+            }
+
+    }
+    return max;
+}
+
+
+
